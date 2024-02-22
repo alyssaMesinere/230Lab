fcee52b31e136bcb7b1e7de12e30153e387e9c93
alyssaMesinere <am00157@mix.wvu.edu>
2024-02-22 10:35:34 -0500

    new commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <h1>CS 230L</h1>
       <h2>Section 002</h2>
       <p>WVU ID 800387816</p>
-      <p>Hi I am Alyssa Mesinere </p>
+      <p>Hi I am Alyssa Mesinere! </p>
     </div>
   );
 }
